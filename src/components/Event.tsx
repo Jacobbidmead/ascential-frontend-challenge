cb2f8b9bab92c918235360d50538503509f0d243
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2025-10-11 22:40:18 +0100

    fix: change timezone displayed in UI to local time zone using datetime_local end point in Event and Events components. - pass timeZone from venue object to formDateTime component to display local time zone

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import {
   Flex,
   Heading,
@@ -12,20 +12,21 @@ import {
   Spinner,
   Button,
   Stack,
-} from '@chakra-ui/react';
-import Breadcrumbs from './Breadcrumbs';
-import Error from './Error';
-import { useSeatGeek } from '../utils/useSeatGeek';
-import { formatDateTime } from '../utils/formatDateTime';
-import { type Venue } from './Events';
+} from "@chakra-ui/react";
+import Breadcrumbs from "./Breadcrumbs";
+import Error from "./Error";
+import { useSeatGeek } from "../utils/useSeatGeek";
+import { formatDateTime } from "../utils/formatDateTime";
+import { type Venue } from "./Events";
 
 interface EventInfoProps {
   event: {
     short_title: string;
     datetime_utc: Date;
+    datetime_local: string;
     venue: Venue;
     url: string;
-  }
+  };
 }
 
 const Event: React.FC = () => {
@@ -39,19 +40,19 @@ const Event: React.FC = () => {
       <Flex justifyContent="center" alignItems="center" minHeight="50vh">
         <Spinner size="lg" />
       </Flex>
-    )
+    );
   }
 
   return (
     <>
-      <Breadcrumbs 
+      <Breadcrumbs
         items={[
-          { label: 'Home', to: '/' },
-          { label: 'Events', to: '/events' },
+          { label: "Home", to: "/" },
+          { label: "Events", to: "/events" },
           { label: event.short_title },
-        ]} 
+        ]}
       />
-      <Flex bgColor='gray.200' p={[4, 6]}>
+      <Flex bgColor="gray.200" p={[4, 6]}>
         <Heading>{event.short_title}</Heading>
       </Flex>
       <EventInfo event={event} />
@@ -61,12 +62,7 @@ const Event: React.FC = () => {
 
 const EventInfo: React.FC<EventInfoProps> = ({ event }) => (
   <Stack spacing="6" m="6">
-    <SimpleGrid 
-      columns={[1, 1, 2]} 
-      borderWidth="1px" 
-      borderRadius="md"
-      p="4" 
-    >
+    <SimpleGrid columns={[1, 1, 2]} borderWidth="1px" borderRadius="md" p="4">
       <Stat>
         <StatLabel display="flex">
           <Box as="span">Venue</Box>
@@ -78,11 +74,16 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => (
         <StatLabel display="flex">
           <Box as="span">Date</Box>
         </StatLabel>
-        <StatNumber fontSize="xl">{formatDateTime(event.datetime_utc)}</StatNumber>
+        <StatNumber fontSize="xl">
+          {" "}
+          {formatDateTime(new Date(event.datetime_local), event.venue.timezone)}
+        </StatNumber>
       </Stat>
     </SimpleGrid>
     <Flex>
-      <Button as={'a'} href={event.url} minWidth="0">Buy Tickets</Button>
+      <Button as={"a"} href={event.url} minWidth="0">
+        Buy Tickets
+      </Button>
     </Flex>
   </Stack>
 );
--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,11 +1,12 @@
-export function formatDateTime(timestamp: Date) {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZoneName: 'short',
+export function formatDateTime(timestamp: Date, timeZone?: string) {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: timeZone,
+    timeZoneName: "short",
   }).format(new Date(timestamp));
 }
